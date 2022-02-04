13a01880f9010eb1865941800b220fe45886bfbb
Josh <josh@mjbrowns.com>
2022-02-03 23:01:30 -0500

    feat: add poc for lib

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='slashReppo',
+    packages=find_packages(include=['slashReppo']),
+    version='0.0.1',
+    description='Discord slash command wrapper',
+    author='Josh Brown, Matt Dray',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
