df56b79f6fd971fc2eb272c947e789f6035691a6
17robots <mdray@ameritech.net>
2022-02-15 22:35:06 -0500

    fix: reorganize code

--- slashReppo/util/interaction.pyi
+++ slashReppo/util/interaction.pyi
@@ -1,10 +1,19 @@
 """Interaction class for responses to commands"""
 
-from pydoc import describe
 from typing import Any, OrderedDict
 from slashReppo.util.bitfield import Bitfield
 from slashReppo.util.user import Member
-from enum import Enum
+from enum import IntEnum
+
+class INTERACTION_CALLBACK_TYPES(IntEnum): ...
+
+class CHANNEL_TYPES(IntEnum): ...
+
+class MESSAGE_COMPONENT_TYPES(IntEnum): ...
+
+class BUTTON_STYLES(IntEnum): ...
+
+class TEX_INPUT_STYLES(IntEnum): ...
 
 class Interaction:
     _type: int = None
@@ -18,14 +27,13 @@ class Interaction:
     def send(autocomplete: Autocomplete) -> None: ...
     def send(modal: Modal) -> None: ...
 
-
-class InteractionData(OrderedDict):
+class InteractionData:
     ...
 
-class InteractionResponse(OrderedDict):
+class InteractionResponse:
     ...
 
-class InteractionCallbackData(OrderedDict):
+class InteractionCallbackData:
     type: int
     data: dict
 
@@ -38,53 +46,58 @@ class Message(InteractionCallbackData):
     message_flags: Bitfield
     components: list(MessageComponent) = None
     attachments: list(Attachment) = None
+    def __init__(self, content: str = None, tts: bool = None, embeds: list(Embed) = None, allows_mentions: list = None, message_flags: Bitfield = None, components: list = None, attachments: list = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
 class Autocomplete(InteractionCallbackData):
     type: int = INTERACTION_CALLBACK_TYPES.AUTOCOMPLETE
+    def __iter__(self) -> dict: ...
 
 class Modal(InteractionCallbackData):
     type: int = INTERACTION_CALLBACK_TYPES.MODAL
     custom_id: str
     title: str
     components: list(MessageComponent)
+    def __init__(self, custom_id: str, title: str, components: list(MessageComponent)) -> None: ...
+    def __iter__(self) -> dict: ...
 
-
-class INTERACTION_CALLBACK_TYPES(Enum):
-    PONG = 1 # ACK a ping
-    CHANNEL_MESSAGE_WITH_SOURCE = 4 # response to an interaction with a message
-    DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5 # ACK an interaction and edit a response later; does not see loading state
-    DEFERRED_UPDATE_MESSAGE = 6 # for components, ACK an interaction and edit the original message later; does not see loading state; only available for component based interactions
-    UPDATE_MESSAGE = 7 # for components, edit the message the component was originally attached to; does not see loading state; only available for component based interactions
-    APPLICATION_COMMAND_AUTOCOMPLETE_RESULT = 8 # respond to an autocomplete interaction with suggestions
-    MODAL = 9 # respond to an interaction with a popup modal; not available for MODAL_SUBMIT and PING interactions
-
-class EmbedFooter(OrderedDict):
+class EmbedFooter:
     text: str = None
     icon_url: str = None
     proxy_icon_url: str = None
+    def __init__(self, text: str = None, icon_url: str = None, proxy_icon_url: str = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class EmbedImage(OrderedDict):
+class EmbedImage:
     url: str = None
     proxy_url: str = None
     height: int = None
     width: int = None
+    def __init__(self, url: str = None, proxy_url: str = None, height: int = None, width: int = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class EmbedProvider(OrderedDict):
+class EmbedProvider:
     name: str = None
     url: str = None
+    def __init__(self, name: str = None, url: str = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
 class EmbedAuthor(OrderedDict):
     name: str = None
     url: str = None
     icon_url: str = None
     proxy_icon_url: str = None
+    def __init__(self, name: str = None, url: str = None, icon_url: str = None, proxy_icon_url: str = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
 class EmbedField:
     name: str = None
     value: str = None
     inline: bool = None
+    def __init__(self, name: str = None, value: str = None, inline: bool = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class Embed(OrderedDict):
+class Embed:
     title: str
     type: str
     description: str
@@ -98,33 +111,26 @@ class Embed(OrderedDict):
     provider: EmbedProvider
     author: EmbedAuthor
     fields: list(EmbedField)
+    def __init__(self, title: str, type: str, description: str, url: str, timestamp: Any, color: int = None, footer: EmbedFooter = None, image: EmbedImage = None, thumbnail: EmbedImage = None, video: EmbedImage = None, provider: EmbedProvider = None, author: EmbedAuthor = None, fields: list(EmbedField) = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class ChannelMention(OrderedDict):
+class ChannelMention:
     id: str = None
     guild_id: str
     type: int
     name: str # name of channel
+    def __init__(self, id: str, guild_id: str, type: int, name: str) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class AllowedMentions(OrderedDict):
+class AllowedMentions:
     parse: list(str)
     roles: list(str)
     users: list(str)
     replied_user: bool
+    def __init__(self, parse: list(str) = None, roles: list(str) = None, users: list(str) = None, replied_user: bool = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class CHANNEL_TYPES(Enum):
-    GUILD_TEXT = 0
-    DM = 1
-    GUILD_VOICE = 2
-    GROUP_DM = 3
-    GUILD_CATEGORY = 4
-    GUILD_NEWS = 5
-    GUILD_STORE = 6
-    GUILD_NEWS_THREAD = 10
-    GUILD_PUBLIC_THREAD = 11
-    GUILD_PRIVATE_THREAD = 12
-    GUILD_STAGE_VOICE = 13
-
-class Attachment(OrderedDict):
+class Attachment:
     id: str
     filename: str
     description: str
@@ -135,15 +141,13 @@ class Attachment(OrderedDict):
     height: int = None
     width: int = None
     ephemeral: bool = None
+    def __init__(self, id: str, filename: str, description: str, content_type: str = None, size: int = None, url: str = None, proxy_url: str = None, height: int = None, width: int = None, ephemeral: bool = None) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class MessageComponent(OrderedDict):
+class MessageComponent:
     type: int
-
-class MESSAGE_COMPONENT_TYPES(Enum):
-    ACTION_ROW = 1
-    BUTTON = 2
-    SELECT_MENU = 3
-    TEXT_INPUT = 4
+    def __init__(self, type: int) -> None: ...
+    def __iter__(self) -> dict: ...
 
 class ActionRow(MessageComponent):
     type: int = MESSAGE_COMPONENT_TYPES.ACTION_ROW
@@ -157,18 +161,15 @@ class Button(MessageComponent):
     custom_id: str
     url: str
     disabled: bool = False
+    def __init__(self, style: int, label: str, emoji: PartialEmoji, custom_id: str, url: str, disabled: bool = False) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class PartialEmoji(OrderedDict):
+class PartialEmoji:
     name: str
     id: str
     animated: bool
-
-class BUTTON_STYLES(Enum):
-    PRIMARY = 1
-    SECONDARY = 2
-    SUCCESS = 3
-    DANGER = 4
-    LINK = 5
+    def __init__(self, name: str, id: str, animated: bool) -> None: ...
+    def __iter__(self) -> dict: ...
 
 class SelectMenu(MessageComponent):
     type: int = MESSAGE_COMPONENT_TYPES.SELECT_MENU
@@ -178,13 +179,17 @@ class SelectMenu(MessageComponent):
     min_values: int
     max_values: int
     disabled: bool = False
+    def __init__(self, custom_id: str, options: list(SelectOption), placeholder: str, min_values: int, max_values: int, disabled: bool = False) -> None: ...
+    def __iter__(self) -> dict: ...
 
-class SelectOption(OrderedDict):
+class SelectOption:
     label: str
     value: str
     description: str
     emoji: PartialEmoji
     default: bool
+    def __init__(self, label: str, value: str, description: str, emoji: PartialEmoji, default: bool) -> None: ...
+    def __iter__(self) -> dict: ...
 
 class TextInput(MessageComponent):
     type: int = MESSAGE_COMPONENT_TYPES.TEXT_INPUT
@@ -196,7 +201,5 @@ class TextInput(MessageComponent):
     required: bool
     value: str
     placeholder: str
-
-class TEX_INPUT_STYLES(Enum):
-    SHORT = 1
-    PARAGRAPH = 2
+    def __init__(self, custom_id: str, style: int, label: str, min_length: int, max_length: int, required: bool, value: str, placeholder: str) -> None: ...
+    def __iter__(self) -> dict: ...
