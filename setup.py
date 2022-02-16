d50340c2cba0998742392bb7654450563c3fe358
Josh <josh@mjbrowns.com>
2022-02-15 22:46:03 -0500

    chore: add requirements to package setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Discord slash command wrapper',
     author='Josh Brown, Matt Dray',
     license='MIT',
-    install_requires=['requests'],
+    install_requires=['requests', 'websockets'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
