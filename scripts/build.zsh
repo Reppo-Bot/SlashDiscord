91a4937d7137d607d1b269c71410a86a5069c71f
Josh <josh@mjbrowns.com>
2022-03-11 23:42:27 -0500

    chore: update version

--- scripts/build.zsh
+++ scripts/build.zsh
@@ -6,5 +6,5 @@ echo y | pip uninstall SlashDiscord
 echo "Removed old lib\n"
 python -m build
 echo "Built lib\n"
-pip install dist/SlashDiscord-0.1.0-py3-none-any.whl
+pip install dist/SlashDiscord-0.1.1-py3-none-any.whl
 echo "Installed new lib\n"
